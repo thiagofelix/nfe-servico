5a21eb5c820bcbc8310449d4dd542fbfc0e55260
Thiago Felix <thiago@thiagofelix.com>
2016-03-20 14:38:19 -0300

    fix(nfe-servico): Testando infra-estrutura de build+deploy ( travis, aws codedeploy )

---
+++ README.md
@@ -0,0 +1,5 @@
+# nfe-servico
+Servidor REST para carregar dados sobre Nota Fiscal de Consumidor Eletrônica
+através do QR Code exibido no DANFE
+
+*Licença: MIT*
---
+++ src/nfe-servico.js
@@ -0,0 +1,12 @@
+import http from 'http'
+
+const port = process.env.PORT || 3000
+
+http.createServer((req, res) => {
+  console.log('%d request received', process.pid)
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('Hello world!\n')
+}).listen(port)
+
+console.log('%d listening on %d', process.pid, port)
+
