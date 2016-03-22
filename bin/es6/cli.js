adc87c161adff218d5d15dec2181d403853eebc0
Thiago Felix <thiago@thiagofelix.com>
2016-03-22 15:07:17 -0300

    fix(cors): Temporariamente habilita cors para todos enderecos

--- bin/es6/cli.js
+++ bin/es6/cli.js
@@ -32,7 +32,7 @@ app.get('/', (req, res) => {
 })
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://nossanota.surge.sh')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
