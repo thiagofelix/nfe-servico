970208fc52726c9113cdf91bc605cc48baea257e
Thiago Felix <thiago@thiagofelix.com>
2016-03-22 14:38:13 -0300

    fix(https): Habilita https com certificado self-signed

--- bin/es6/cli.js
+++ bin/es6/cli.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 
-import http from 'http'
+import https from 'https'
 import alloc from 'tcp-bind'
 import minimist from 'minimist'
 import isRoot from 'is-root'
 import compression from 'compression'
 import express from 'express'
+import fs from 'fs'
+import path from 'path'
 import { consultar } from 'nfe-biblioteca'
 
 const argv = minimist(process.argv.slice(2), {
   alias: { p: 'port', u: 'uid', g: 'gid' },
-  default: { port: isRoot() ? 80 : 8000 }
+  default: { port: isRoot() ? 443 : 8000 }
 })
 
 const fd = alloc(argv.port)
@@ -35,19 +37,18 @@ app.use((req, res, next) => {
   next()
 })
 
-
 app.get(/nfe/, (req, res) => {
   let link = req.originalUrl.slice(5)
   consultar(link)
-    .then(nfe => {
-      res.status(200).json(nfe)
-    })
-    .catch(err => {
-      res.status(500).json({ error: err })
-    })
+    .then(nfe => res.status(200).json(nfe))
+    .catch(error => res.status(500).json({error}))
 })
 
-const server = http.createServer(app)
+const options = {
+  key: fs.readFileSync(path.join('deploy', 'key.pem')),
+  cert: fs.readFileSync(path.join('deploy', 'cert.pem'))
+}
+const server = https.createServer(options, app)
 server.listen({fd}, () => {
   console.log('process pid=%d uid=%d gid=%d', process.pid, process.getuid(), process.getgid())
   console.log('listening on: %s', JSON.stringify(server.address()))
