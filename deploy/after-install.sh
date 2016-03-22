65f311185e6c152f6952c2b32fb1ec72bba00fca
Thiago Felix <thiago@thiagofelix.com>
2016-03-21 23:25:42 -0300

    chore(codedeploy): Garante instalacao do nodejs

--- deploy/after-install.sh
+++ deploy/after-install.sh
@@ -1,3 +1,10 @@
+echo "Installing Node...";
+curl --silent --location https://rpm.nodesource.com/setup_5.x | bash -
+yum -y install nodejs
+
 echo "Installing application...";
 cd /srv/src/
 npm install
+
+echo "Building application...";
+npm run build
